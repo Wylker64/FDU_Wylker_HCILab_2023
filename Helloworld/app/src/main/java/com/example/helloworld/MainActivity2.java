2b6d0c74a93c8356f8be48aee7dffe1451c15386
Wylker <wylker@qq.com>
2023-09-16 22:53:02 +0800

    [new]Added Activity 2. Tested button function.
    TODO: test text filling function

--- Helloworld/app/src/main/java/com/example/helloworld/MainActivity2.java
+++ Helloworld/app/src/main/java/com/example/helloworld/MainActivity2.java
@@ -7,21 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        Button buttonNavigate = findViewById(R.id.buttonNavigate);
-        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
 
     }
 }
